35f1a723b78a9edbb97efa49439498a1da4ce047
林幸嗣 <koji06099@gmail.com>
2021-03-04 00:06:21 +0900

    redirect設定

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,11 +56,12 @@ const useStyles = makeStyles({
 const Main = () => {
   // require('dotenv').config();
   useEffect(() => {
+    console.log(window.location.href)
     const domein = document.domain
     const REACT_APP_HEROKU_DOMEIN = process.env.REACT_APP_HEROKU_DOMEIN;
     const REACT_APP_ORIGINAL_URL = process.env.REACT_APP_ORIGINAL_URL;
     const REACT_APP_INSECURE_URL = process.env.REACT_APP_INSECURE_URL;
-    if ((domein===REACT_APP_HEROKU_DOMEIN)||(window.location.href===REACT_APP_INSECURE_URL)) window.location.href=REACT_APP_ORIGINAL_URL
+    if ((window.location.href===REACT_APP_INSECURE_URL) || (domein===REACT_APP_HEROKU_DOMEIN)) window.location.href=REACT_APP_ORIGINAL_URL
   }, [])
   const classes = useStyles();
 
