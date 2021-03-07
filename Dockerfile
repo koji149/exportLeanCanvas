1e7efc50b873cf8dc3b3da1f047b678454af77da
林幸嗣 <koji06099@gmail.com>
2021-03-08 00:08:24 +0900

    edit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,3 @@
 FROM node:14.15.5
 
 WORKDIR /exportLeanCanvas
-COPY ./web/package*.json ./
-RUN ls
-RUN npm install
-COPY . .
--- web/src/pages/Page404.js
+++ web/src/pages/Page404.js
@@ -5,7 +5,6 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import { Link } from "react-router-dom";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import errorImg from "../image/404.png";
 
